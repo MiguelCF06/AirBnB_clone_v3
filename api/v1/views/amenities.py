27893e3554590074eb05acc162aa393ca9e80e58
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-09-09 18:18:54 +0000

    [Update]Added amenity import

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,6 +7,7 @@ from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.city import City
 from models.state import State
+from models.amenity import Amenity
 
 
 @app_views.route("/amenities/", methods=["GET"], strict_slashes=False)
